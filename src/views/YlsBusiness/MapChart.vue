5bba790c8e4efd686f6cd0b1e92373a2916713ef
yls <2689623200@qq.com>
2020-09-23 23:17:26 +0800

    添加中国地图

--- src/views/YlsBusiness/MapChart.vue
+++ src/views/YlsBusiness/MapChart.vue
@@ -89,46 +89,46 @@
         }
 
         //定义全国省份的数组
-        // var provinces = {
-        //   // 23个省
-        //   台湾: "taiwan",
-        //   河北: "hebei",
-        //   山西: "shanxi",
-        //   辽宁: "liaoning",
-        //   吉林: "jilin",
-        //   黑龙江: "heilongjiang",
-        //   江苏: "jiangsu",
-        //   浙江: "zhejiang",
-        //   安徽: "anhui",
-        //   福建: "fujian",
-        //   江西: "jiangxi",
-        //   山东: "shandong",
-        //   河南: "henan",
-        //   湖北: "hubei",
-        //   湖南: "hunan",
-        //   广东: "guangdong",
-        //   海南: "hainan",
-        //   四川: "sichuan",
-        //   贵州: "guizhou",
-        //   云南: "yunnan",
-        //   陕西: "shanxi1",
-        //   甘肃: "gansu",
-        //   青海: "qinghai",
-        //   // 5个自治区
-        //   新疆: "xinjiang",
-        //   广西: "guangxi",
-        //   内蒙古: "neimenggu",
-        //   宁夏: "ningxia",
-        //   西藏: "xizang",
-        //   // 4个直辖市
-        //   北京: "beijing",
-        //   天津: "tianjin",
-        //   上海: "shanghai",
-        //   重庆: "chongqing",
-        //   // 2个特别行政区
-        //   香港: "xianggang",
-        //   澳门: "aomen"
-        // };
+        var provinces = {
+          // 23个省
+          台湾: "taiwan",
+          河北: "hebei",
+          山西: "shanxi",
+          辽宁: "liaoning",
+          吉林: "jilin",
+          黑龙江: "heilongjiang",
+          江苏: "jiangsu",
+          浙江: "zhejiang",
+          安徽: "anhui",
+          福建: "fujian",
+          江西: "jiangxi",
+          山东: "shandong",
+          河南: "henan",
+          湖北: "hubei",
+          湖南: "hunan",
+          广东: "guangdong",
+          海南: "hainan",
+          四川: "sichuan",
+          贵州: "guizhou",
+          云南: "yunnan",
+          陕西: "shanxi1",
+          甘肃: "gansu",
+          青海: "qinghai",
+          // 5个自治区
+          新疆: "xinjiang",
+          广西: "guangxi",
+          内蒙古: "neimenggu",
+          宁夏: "ningxia",
+          西藏: "xizang",
+          // 4个直辖市
+          北京: "beijing",
+          天津: "tianjin",
+          上海: "shanghai",
+          重庆: "chongqing",
+          // 2个特别行政区
+          香港: "xianggang",
+          澳门: "aomen"
+        };
 
         // 市
         var cityMap = {
@@ -483,40 +483,38 @@
         // 点击触发
         myChart.on("click", param => {
           console.log(param)
-          let names = param.name;
-          showProvince(names,names);
-          // if (param.name in provinces) {
-          //   // 处理省模块
-          //   let names = param.name;
-            // for (let key in provinces) {
-            //   if (names == key) {
-            //     showProvince(provinces[key], key);
-            //     break;
-            //   }
-            // }
-            //showProvince(names,names);
+          // let names = param.name;
+          // showProvince(names,names);
+          if (param.name in provinces) {
+            // 处理省模块
+            let names = param.name;
+            for (let key in provinces) {
+              if (names == key) {
+                showProvince(provinces[key], key);
+                break;
+              }
+            }
+            // showProvince(names,names);
 
-          // } else if (param.name in cityMap) {
-          //   // 处理市模块
-          //   let names = param.name;
-          //   for (let key in cityMap) {
-          //     if (names == key) {
-          //       showCitys(cityMap[key], key);
-          //       break;
-          //     }
-          //   }
-          // }
+          } else if (param.name in cityMap) {
+            // 处理市模块
+            let names = param.name;
+            for (let key in cityMap) {
+              if (names == key) {
+                showCitys(cityMap[key], key);
+                break;
+              }
+            }
+          }
         });
 
         //展示对应的省
         function showProvince(cName, param) {
-          console.log(param)
           initImportFile(param);
           initEcharts(param);
         }
 
         function initImportFile(pro) {
-          console.log(pro)
           var filaName = {
             浙江: "zhejiang.js",
             安徽: "anhui.js",
@@ -559,11 +557,9 @@
 
         //展示对应市
         function showCitys(cName, param) {
-          console.log(cName, param)
           // 显示县级地图
-          $.getJSON(`/map/city/${cName}.json`, data => {
+          $.getJSON(`static/city/${cName}.json`, data => {
             that.$echarts.registerMap(param, data);
-            alert("县")
             initEcharts(param);
           })
         }
