5429c511fb46144c9ed5c2cde44017d7e2e82a38
yls <2689623200@qq.com>
2020-10-09 22:54:55 +0800

    项目规范化

--- src/views/Core/YlsDateTime.vue
+++ src/views/Core/YlsDateTime.vue
@@ -9,18 +9,6 @@
       end-placeholder="结束日期">
     </el-date-picker>
   </div>
-  <!-- <div class="block">
-    <span class="demonstration">带快捷选项</span>
-    <el-date-picker
-      v-model="value2"
-      type="datetimerange"
-      :picker-options="pickerOptions"
-      range-separator="至"
-      start-placeholder="开始日期"
-      end-placeholder="结束日期"
-      align="right">
-    </el-date-picker>
-  </div> -->
 </template>
 
 <script>
